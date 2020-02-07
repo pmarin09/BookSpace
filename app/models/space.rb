32faa805f2b4a717850de8aa44dbd2943f7b3b25
Patrick <patrickmarin@gmail.com>
2020-02-07 15:10:21 -0500

    BooskSpace App

---
+++ app/models/space.rb
@@ -0,0 +1,4 @@
+class Space < ApplicationRecord
+  def index
+  end
+end
---
+++ db/migrate/20200207200742_create_coworking_space.rb
@@ -0,0 +1,13 @@
+class CreateCoworkingSpace < ActiveRecord::Migration[6.0]
+  def change
+    create_table :coworking_spaces do |t|
+        t.integer :user_id
+        t.string :name
+        t.text :address
+        t.string :city
+        t.integer :spaces_qty
+
+        t.timestamps
+    end
+  end
+end
